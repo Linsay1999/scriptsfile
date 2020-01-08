78b2129a17311386eba53cf973193c17a5b1bc85
linsay1999 <dzalialfred@gmail.com>
2020-01-08 18:58:55 +0000

    12th class script_ifcondition

---
+++ script12_ifcondition.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Description: IF CONDITION STATEMENT
+# Author: Alfred
+# Date 
+# Ref.: W9D2
+
+clear
+
+if
+ [ 3 -eq 5 ]
+
+then 
+
+echo "3 is bigger than five"
+
+else
+
+echo "3 is smaller than five"
+
+echo "Thank you"
+
+fi
