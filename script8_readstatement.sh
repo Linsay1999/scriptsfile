2cb7fa5aa03202eff406b8ad5aad84f230b4c867
linsay1999 <dzalialfred@gmail.com>
2020-01-08 18:12:24 +0000

    8th class script-read statement concept

---
+++ script8_readstatement.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Description: Read Statement Concept
+# Author: Alfre
+# Date : Nov.2019
+# Ref.: W9D2
+
+echo "What is your name?"
+
+read N
+
+echo "What is your city?"
+
+read T
+
+echo "What is your state?"
+
+read X
+
+sleep 5
+
+echo "Your name is $N, your city is $T and your state is $X"
+
