604d751adc27872f1aa7882803f506e77fe87a19
linsay1999 <dzalialfred@gmail.com>
2020-01-08 17:23:21 +0000

    4th class script-echo concept

---
+++ script4_helloproject.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Description: Project on echo command
+# Author : Alfred DZALI
+# Date: October 19
+# Details: This is are some lines only echo command
+
+echo "Hello Serge"
+echo "Welcome to the scripting classe"
+echo "Hope you are having fun learning"
+echo "Please visit aisle 13 for our specials"
