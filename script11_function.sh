ada3c6b3f2c82c0c0deffeffb21b5d1724316a0b
linsay1999 <dzalialfred@gmail.com>
2020-01-08 18:40:17 +0000

    11th class script_function concept

---
+++ script11_function.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Description : Etude de la notion de function
+# Author: Alfred
+# Date : Nov.2019
+# Ref.: W9D2
+
+#Syntax needs to have the name of the function followed by couple parentheses. 
+#The arguments of the functions should then be mentionned between a curlybracket
+#To call the function we just type the variable related to it
+
+system() {
+uname
+uptime
+cal
+w
+}
+
+system
