256e3bc58e9a0f9b206fa7f69f6fce6a711faceb
linsay1999 <dzalialfred@gmail.com>
2020-01-08 18:24:24 +0000

    flag e (-e)_empty interligne

---
+++ script10_interligne.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Description: Read Statement Concept
+# Author: Alfre
+# Date : Nov.2019
+# Ref.: W9D2
+
+echo -en "\nWhat is your name ?\n "
+
+read N
+
+echo -en "\nWhat is your city ?\n "
+
+read T
+
+echo -en "\nWhat is your state ?\n "
+
+read X
+
+sleep 5
+
+echo "Your name is $N, your city is $T and your state is $X"
+
