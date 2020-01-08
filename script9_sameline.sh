3f775d68309d2743982001efe99febcca2edc995
linsay1999 <dzalialfred@gmail.com>
2020-01-08 18:13:47 +0000

    9th class script- sameline (-n)

---
+++ script9_sameline.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Description: Read Statement Concept
+# Author: Alfre
+# Date : Nov.2019
+# Ref.: W9D2
+
+echo -n "What is your name?"
+
+read N
+
+echo -n "What is your city?"
+
+read T
+
+echo -n "What is your state?"
+
+read X
+
+sleep 5
+
+echo "Your name is $N, your city is $T and your state is $X"
+
