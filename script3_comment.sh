4c1fb6d5fdddaed2941fa92599f5d132b172d8aa
linsay1999 <dzalialfred@gmail.com>
2020-01-08 17:22:22 +0000

    3rd class comment concept

---
+++ script3_comment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Description: First Script
+# Author: Alfred
+# Date: oct 2019
+# Details: This script is introduction to the comment lines.That are these lines that are commented out giving some description to the script.
+
+echo "Hello World"
