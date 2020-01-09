2c15e15cfadf6aa19793988458922336ef370a4c
linsay1999 <dzalialfred@gmail.com>
2020-01-09 01:32:27 +0000

    18th class script_forloop

---
+++ script18th_forloup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Description: For Loop
+# Author: Alfred
+# Date:
+# Ref.: W12D2
+
+for items in red black yellow brown orange white pink purple;
+do
+
+  echo "the color picked is $items "
+  sleep 3
+
+done
